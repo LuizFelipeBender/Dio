5ad01f21b58a21523b2d9e3be607fc8c41c6ea76
LuizBender@outlook.com.br <LuizBender@outlook.com.br>
2022-03-09 22:34:41 -0300

    first commit

---
+++ Impulso Fullstack Web Developer/Desafios Impulso Intermediários - C#/Desafio01/Desafio_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//TODO: Complete os espaços em branco com uma solução possível para o problema.
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dias = int.Parse(Console.ReadLine());
+            var anos = dias  ;
+            dias = dias   ;
+            var meses = dias;
+            dias = dias;
+
+            Console.WriteLine($"{anos} ano(s)");
+            Console.WriteLine($"{meses} mes(es)");
+            Console.WriteLine($"{dias} dia(s)");
+        }
+    }
+}
